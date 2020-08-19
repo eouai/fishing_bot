c7f07d8aed2368027706be606d2dfb4e060a003c
eouai <burtgwilliam@byu.net>
2020-08-19 10:16:00 -0400

    update use_protobuf with relative path references

--- use_protobuf.py
+++ use_protobuf.py
@@ -1,21 +1,21 @@
 import os
 import sys
 
-current_path = os.getcwd()
-# sys.path.append(current_path)
-# sys.path.append(current_path + "\\tensorflow\\models\\research\\")
-# sys.path.append(current_path + "\\tensorflow\\models\\research\\object_detection\\utils")
-os.chdir(current_path + '/tensorflow/models/research/')
-print(os.getcwd())
-
 # args = sys.argv
-# directory = args[1]
+# dir = args[1]
 # protoc_path = args[2]
-directory = 'object_detection/protos/'
-protoc_path = current_path + '/bin/protoc'
+this_path = os.path.abspath(os.path.dirname(__file__))
+print(this_path)
+operating_dir = 'C:/Users/Burt/Documents/Git/fishing_bot/models/research/'
+protos_abs = 'C:/Users/Burt/Documents/Git/fishing_bot/models/research/object_detection/protos/'
+protos_rel = 'object_detection/protos/'
+protoc_path = 'C:/Users/Burt/Documents/Git/fishing_bot/bin/protoc.exe'
+sys.path.append("C:\\Users\\Burt\\Documents\\Git\\fishing_bot\\models\\research\\")
+sys.path.append("C:\\Users\\Burt\\Documents\\Git\\fishing_bot\\models\\object_detection\\utils")
+os.chdir(operating_dir)
 
-for file in os.listdir(directory):
+
+for file in os.listdir(protos_abs):
     if file.endswith(".proto"):
-        os.system(protoc_path+" "+directory+file+" --python_out=.")  # --proto_path="+directory)
-
-print('protos files successfully generated')
+        os.system(protoc_path+" "+protos_rel+file+" --python_out=.") #+" --proto_path="+protos_dir)
+        print(protoc_path+" "+protos_rel+file+" --python_out=.") #+protos_dir+" --proto_path="+protos_dir)
