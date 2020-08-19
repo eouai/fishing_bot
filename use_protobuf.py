4e7e156a8b377589a57570d20e60e2efc7d5eac7
eouai <burtgwilliam@byu.net>
2020-08-19 10:18:27 -0400

    updated correct proto script, removed old

--- use_protobuf.py
+++ use_protobuf.py
@@ -1,21 +1,21 @@
 import os
 import sys
 
-# args = sys.argv
-# dir = args[1]
-# protoc_path = args[2]
+# Assumes
 this_path = os.path.abspath(os.path.dirname(__file__))
-print(this_path)
-operating_dir = 'C:/Users/Burt/Documents/Git/fishing_bot/models/research/'
-protos_abs = 'C:/Users/Burt/Documents/Git/fishing_bot/models/research/object_detection/protos/'
+# this_path = 'C:\\Users\\Burt\\Documents\\Git\\fishing_bot'
+protoc_path = os.path.join(this_path, 'bin\\protoc.exe')
+operating_dir = os.path.join(this_path, 'models\\research')
+protos_abs = os.path.join(operating_dir, 'object_detection\\protos')
 protos_rel = 'object_detection/protos/'
-protoc_path = 'C:/Users/Burt/Documents/Git/fishing_bot/bin/protoc.exe'
-sys.path.append("C:\\Users\\Burt\\Documents\\Git\\fishing_bot\\models\\research\\")
-sys.path.append("C:\\Users\\Burt\\Documents\\Git\\fishing_bot\\models\\object_detection\\utils")
 os.chdir(operating_dir)
+
+# sys.path.append(operating_dir)
+# sys.path.append("C:\\Users\\Burt\\Documents\\Git\\fishing_bot\\models\\object_detection\\utils")
+
 
 
 for file in os.listdir(protos_abs):
     if file.endswith(".proto"):
         os.system(protoc_path+" "+protos_rel+file+" --python_out=.") #+" --proto_path="+protos_dir)
-        print(protoc_path+" "+protos_rel+file+" --python_out=.") #+protos_dir+" --proto_path="+protos_dir)
+        # print(protoc_path+" "+protos_rel+file+" --python_out=.") #+protos_dir+" --proto_path="+protos_dir)
